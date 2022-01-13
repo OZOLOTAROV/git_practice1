1f88c15e499efdebad0e101f2bdbf7221013f3e6
OZOLOTAROV <oleksandrzolotarov94@gmail.com>
2022-01-12 20:20:21 -0600

    Work 3 is done and about to be pushed into Github after submit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 
     // Doing awesome work 2!
 
-
+    // Diing awesome work 3 !
+    // AWESOME WORK 3 IS DONE and ready to be committed
+    // And ready to be pushed to remote repository
 
 }
